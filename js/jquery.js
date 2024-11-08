f9da0675ef0d9b2ea9c399f788ddbb9042df1288
MrPesk84 <mrpesk84@gmail.com>
2024-11-08 09:33:49 +0100

    Update: index

---
+++ js/jquery.js
@@ -0,0 +1,12 @@
+// Seleccionar la imagen por su ID
+const imagen = document.getElementById("imagen-efecto");
+
+// Añadir efecto al pasar el cursor (reduce opacidad)
+imagen.addEventListener("mouseover", () => {
+  imagen.style.opacity = "0.5";
+});
+
+// Quitar efecto al retirar el cursor (restablece opacidad)
+imagen.addEventListener("mouseout", () => {
+  imagen.style.opacity = "1";
+});
